543c8becb40eef559adb1a434c419212b86bc0f1
Hannes Körber <hannes@hkoerber.de>
2023-11-09 14:35:36 +0100

    Make first solution more straightforward

--- 1.py
+++ 1.py
@@ -1,31 +1,21 @@
-from pprint import pprint
+# s = so, w = wie
 l = "swssssswswwwssssswwsssswswwssswwswwswsssswwsswsw"
 
-count_so = sum([1 if c == "s" else 0 for c in l])
-print(count_so)
+# we have 48 "bits"
+# those are 6 x 8 bits that each decode via ASCII to a char
 
-count_wie = sum([1 if c == "w" else 0 for c in l])
-print(count_wie)
-
+# convert s/w to bits
 l = [0 if c == "s" else 1 for c in l]
 
-chunks = 3
-digits_per_chunk = 2
+# chunk into 8 bit chunks
+l = [l[i:i + 8] for i in range(0,len(l),8)]
 
-size = int(len(l) / chunks)
+# convert the 8 bits chunks into integers
+l = [int("".join([str(b) for b in c]), 2) for c in l]
 
-l = [l[i:i + size] for i in range(0,len(l),size)]
+# convert from int to char via ascii
+l = [chr(c) for c in l]
 
-pprint(l)
-l = [[c[i:i + 8] for i in range(0,len(c),8)] for c in l]
-
-pprint(l)
-
-l = [["".join([str(b) for b in d]) for d in c] for c in l]
-
-l = [[int(b, 2) for b in c] for c in l]
-
-l = ["".join([chr(b) for b in c]) for c in l]
-
-pprint(l)
-
+# concat and print the result
+result = "".join(l)
+print(result)
